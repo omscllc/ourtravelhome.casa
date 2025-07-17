e227cb7fcf16f0cb9e592788dc3ab8bc78e23007
www-data <www-data@server1.challengecoinmuseum.site>
2025-07-17 03:11:35 +0000

    first commit

---
+++ web/themes/our_travel_home/js/our-travel-home.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Our Travel Home behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.ourTravelHome = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
